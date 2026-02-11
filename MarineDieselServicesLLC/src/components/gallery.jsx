46b6dc3c4c45b85c38035a60a7ffc4d23e8d0874
manje22 <uvodicmariela@gmail.com>
2026-02-11 18:34:07 +0100

    create slideshow component

--- MarineDieselServicesLLC/src/components/gallery.jsx
+++ MarineDieselServicesLLC/src/components/gallery.jsx
@@ -2,70 +2,79 @@ import { useState } from "react";
 
 
 function Gallery() {
-    const images = [
+  const images = [
     {
-        src: "/images/8VA132904 (3).jpg.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/8VA132904 (3).jpg.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/4679464.jpg.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/4679464.jpg.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/DSC03901.JPG.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/DSC03901.JPG.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/IMG_20200616_1456310.jpg.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/IMG_20200616_1456310.jpg.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/IMG-20240117-WA0001.jpg.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/IMG-20240117-WA0001.jpg.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/IMG-20240218-WA0036.jpg.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/IMG-20240218-WA0036.jpg.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/P1141383.JPG.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/P1141383.JPG.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/P8229961.JPG.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/P8229961.JPG.jpeg",
+      width: 1500,
+      height: 1000,
     },
     {
-        src: "/images/PB090439.JPG.jpeg",
-        width: 1500,
-        height: 1000,
+      src: "/images/PB090439.JPG.jpeg",
+      width: 1500,
+      height: 1000,
     },
-    {
-        src: "/images/Rebuilt engine.jpg.jpeg",
-        width: 1500,
-        height: 1000,
-    },
-    ];
+  ];
 
- 
+  //Our State >>>
+  const [nextIndex, setNextIndex] = useState(0);
+
+  //Update "images" to your array name >>>
+  const arrayLength = images.length;
+
+  //Our setTimeout method >>>
+  setTimeout(() => {
+    if (nextIndex < arrayLength - 1) {
+      setNextIndex(nextIndex + 1);
+    } else {
+      setNextIndex(0);
+    }
+  }, 3000);
+
   return (
-    <>
-        <div className="grid grid-cols-6 gap-4">
-            {images.map((image, index) => (
-                <div className="max-w-7xl mx-auto grid grid-cols-3 gap-6">
-                    <img src={image.src} alt={`Image ${index}`} className="w-full h-auto object-cover" />
-                </div>
-            ))}
-        </div>
-    </>
+    <div id="slideshow-container">
+      <div id="slideshow-image">
+        <img
+          style={{ width: 1500, height: 1000 }}
+          src={images[nextIndex].src}
+          alt="Nick"
+        ></img>
+      </div>
+    </div>
   );
 }
 
--- MarineDieselServicesLLC/src/pages/Home.jsx
+++ MarineDieselServicesLLC/src/pages/Home.jsx
@@ -4,16 +4,19 @@ import Gallery from "../components/gallery";
 function Home() {
 
   return (
-    <>
-        <div className="absolute top-0 left-0 text-left ml-5 mt-5 ">
+    <div className="flex flex-col justify-center gap-20">
+        <div className="text-left mt-5 gap-5 flex flex-col">
             <h2 className="text-8xl">Marine Diesel Services LLC</h2>
-            <h1 className="text-8xl">DIESEL ENGINE SERVICE, SALES & PARTS SALES</h1>
-            <h3 className="text-3xl">VOLVO, STEYR, YAAR, CUMMINS, CATEPILLAR, IVECO, MTU, BUKH, DETROIT, NANNI, LISTER, DEERE</h3>
+            <h1 className="text-5xl">DIESEL ENGINE SERVICE, SALES & PARTS SALES</h1>
+            <h3 className="text-3xl font-extrabold">VOLVO, STEYR, YAAR, CUMMINS, CATEPILLAR, IVECO, MTU, BUKH, DETROIT, NANNI, LISTER, DEERE</h3>
         </div>
         <div>
             <Gallery/>
         </div>
-        <div className="absolute bottom-0 left-0 mb-5 ml-5">
+        <div className="mb-5 ml-5">
+            <div className="text-xl text-left mb-5">
+                <h1>Contact us:</h1>
+            </div>
             <div className="text-2xl text-left">
                 <p>1835 E HALLANDALE BEACH BLVD 175</p>
                 <p>HALLANDALE BEACH, FL 33009</p>
@@ -21,7 +24,7 @@ function Home() {
                 <p>sergio@marinedieselservices.comcastbiz.net</p>
             </div>
         </div>
-    </>
+    </div>
   );
 }
 
