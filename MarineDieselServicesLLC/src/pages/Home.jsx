e96e0e138c72105bcda0ffda6c214e8bfd9f17da
manje22 <uvodicmariela@gmail.com>
2026-02-11 19:51:02 +0100

    text modifications

--- MarineDieselServicesLLC/src/pages/Home.jsx
+++ MarineDieselServicesLLC/src/pages/Home.jsx
@@ -6,22 +6,22 @@ function Home() {
   return (
     <div className="flex flex-col justify-center gap-20">
         <div className="text-left mt-5 gap-5 flex flex-col">
-            <h2 className="text-8xl">Marine Diesel Services LLC</h2>
-            <h1 className="text-5xl">DIESEL ENGINE SERVICE, SALES & PARTS SALES</h1>
-            <h3 className="text-3xl font-extrabold">VOLVO, STEYR, YAAR, CUMMINS, CATEPILLAR, IVECO, MTU, BUKH, DETROIT, NANNI, LISTER, DEERE</h3>
+            <h2 className="text-5xl">Marine Diesel Services LLC</h2>
+            <h1 className="text-3xl">DIESEL ENGINE SERVICE, SALES & PARTS SALES</h1>
+            <h3 className="text-xl font-extrabold">VOLVO, STEYR, YANMAR, CUMMINS, CATEPILLAR, IVECO, MTU, BUKH, DETROIT, NANNI, LISTER, DEERE</h3>
         </div>
         <div>
             <Gallery/>
         </div>
         <div className="mb-5 ml-5">
-            <div className="text-xl text-left mb-5">
+            <div className="font-bold text-left mb-5">
                 <h1>Contact us:</h1>
             </div>
             <div className="text-2xl text-left">
+                <p>sergio@marinedieselservices.comcastbiz.net</p>
+                <p>Tel: (954) 326-7755</p>
                 <p>1835 E HALLANDALE BEACH BLVD 175</p>
                 <p>HALLANDALE BEACH, FL 33009</p>
-                <p>Tel: (954) 326-7755</p>
-                <p>sergio@marinedieselservices.comcastbiz.net</p>
             </div>
         </div>
     </div>
